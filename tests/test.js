57d9f41fbacb78f9701c92e0af450d5ca8b45de4
poornima <pvenkatakrishnan@paypal.com>
2014-08-18 14:39:06 -0700

    Rewrite parser differently

--- tests/test.js
+++ tests/test.js
@@ -1,18 +1,21 @@
 'use strict';
-var transposer = require('../index')(),
+var Transposer = require('../lib/transposer'),
+    jsonPath = require ('JSONPath'),
     test = require('tape');
 
 test('logger', function (t) {
 
     t.test('should be able to parse simple object refs', function (t) {
         var obj = {
-            'a': {
-                'b': {
-                    'c' : 'Found'
+                'a': {
+                    'b': {
+                        'c' : 'Found'
+                    }
                 }
-            }
-        };
-        t.deepEqual(transposer.eval(obj, '$.a.b.c'), ['Found']);
+            },
+            tr = new Transposer('$.a.b.c');
+        t.deepEqual(tr.eval(obj), ['Found']);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.a.b.c'));
         t.end();
     });
 
@@ -25,8 +28,12 @@ test('logger', function (t) {
                 'c': [1,2,3],
                 'd': 'no'
             }
-        };
-        t.deepEqual(transposer.eval(obj, '$.a.*'), [{'h': 'yes'}, [1,2,3], 'no']);
+            },
+            tr = new Transposer('$.a.*');
+
+        t.deepEqual(tr.eval(obj), [{'h': 'yes'}, [1,2,3], 'no']);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.a.*'));
+
         t.end();
     });
 
@@ -42,159 +49,168 @@ test('logger', function (t) {
                 'l': 'no',
                 'blue': false
             }
-        };
-        t.deepEqual(transposer.eval(obj, '$.*.blue'), [true, false]);
+            },
+            tr = new Transposer('$.*.blue');
+
+        t.deepEqual(tr.eval(obj), [true, false]);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.*.blue'));
+
         t.end();
     });
 
     t.test('should test another combination pattern of * an .', function(t) {
         var obj = {
-            'b': {
-                'h':'yes',
-                'blue': {
-                    'green': {'1': '2'}
+                'b': {
+                    'h':'yes',
+                    'blue': {
+                        'green': {'1': '2'}
+                    }
+                },
+                'c': [1,2,3],
+                'd': 'no',
+                'e': {
+                    'l': 'no',
+                    'blue': [5,6,7]
                 }
             },
-            'c': [1,2,3],
-            'd': 'no',
-            'e': {
-                'l': 'no',
-                'blue': [5,6,7]
-            }
-        };
-        t.deepEqual(transposer.eval(obj, '$.*.blue.*'), [{'1': '2'}]);
+            tr = new Transposer('$.*.blue.*');
+
+        t.deepEqual(tr.eval(obj), [ { '1': '2' }, 5, 6, 7 ]);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.*.blue.*'));
+
         t.end();
     });
 
-    t.test('should test for simple array - with *', function(t) {
+
+    t.test('should test for simple array - with idx *', function(t) {
         var obj = {
-            'b': {
-                'h': [
-                    {1: 2},
-                    [3,4,5],
-                    'blue',
-                    true
-                ]
-            }
-        };
-        t.deepEqual(transposer.eval(obj, '$.b.h[*]'), [{1: 2}, [3,4,5], 'blue', true]);
+                'b': {
+                    'h': [
+                        {1: 2},
+                        [3,4,5],
+                        'blue',
+                        true
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h[*]');
+
+        t.deepEqual(tr.eval(obj), [{1: 2},
+            [3,4,5],
+            'blue',
+            true]);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h[*]'));
+
+        t.end();
+    });
+
+    t.test('should test for simple array - with multi idx', function(t) {
+        var obj = {
+                'b': {
+                    'h': [
+                        {1: 2},
+                        [3,4,5],
+                        'blue',
+                        true
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h[1,2]');
+        t.deepEqual(tr.eval(obj), [[3,4,5],'blue']);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h[1,2]'));
+
         t.end();
     });
 
     t.test('should test for simple array - with single idx', function(t) {
         var obj = {
-            'b': {
-                'h': [
-                    {1: 2},
-                    [3,4,5],
-                    'blue',
-                    true
-                ]
-            }
-        };
-        t.deepEqual(transposer.eval(obj, '$.b.h[1]'), [3,4,5]);
+                'b': {
+                    'h': [
+                        {1: 2},
+                        [3,4,5],
+                        'blue',
+                        true
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h[1]');
+        t.deepEqual(tr.eval(obj, '$.b.h[1]'), [[3,4,5]]);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h[1]'));
+
         t.end();
     });
 
     t.test('should test for simple array - with multi idx', function(t) {
         var obj = {
-            'b': {
-                'h': [
-                    {1: 2},
-                    [3,4,5],
-                    'blue',
-                    true
-                ]
-            }
-        };
-        t.deepEqual(transposer.eval(obj, '$.b.h[1,2]'), [[3,4,5],'blue']);
+                'b': {
+                    'h': [
+                        {foo: [1,2,3]},
+                        {foo: [4,5,6]},
+                        {foo: {color: 'blue'}},
+                        true
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h[*].foo[*]');
+
+        t.deepEqual(tr.eval(obj), [1,2,3,4,5,6, 'blue']);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h[*].foo[*]'));
+
         t.end();
     });
 
-    t.test('should test for simple array - with multi idx', function(t) {
+    t.test('should test referencing the length of array', function(t) {
         var obj = {
-            'b': {
-                'h': [
-                    {1: 2},
-                    [3,4,5],
-                    'blue',
-                    true
-                ]
-            }
-        };
-        t.deepEqual(transposer.eval(obj, '$.b.h[1,2]'), [[3,4,5],'blue']);
+                'b': {
+                    'h': [
+                        {foo: [1,2,3]},
+                        {foo: [4,5,6]},
+                        'blue',
+                        true
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h.length');
+        t.deepEqual(tr.eval(obj), [4]);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h.length'));
+
         t.end();
     });
 
-    t.test('should test for simple array - with multi idx', function(t) {
+    t.test('should test referencing a specific item in the array using length and @', function(t) {
         var obj = {
-            'b': {
-                'h': [
-                    {foo: [1,2,3]},
-                    {foo: [4,5,6]},
-                    'blue',
-                    true
-                ]
-            }
-        };
-        t.deepEqual(transposer.eval(obj, '$.b.h[*].foo[*]'), [1,2,3,4,5,6]);
-        t.end();
-    });
+                'b': {
+                    'h': [
+                        {foo: [1,2,3]},
+                        {foo: [4,5,6]},
+                        'blue',
+                        true
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h[(@.length-2)]');
 
-    t.test('should test referencing a specific item in array using @', function(t) {
-        var obj = {
-            'b': {
-                'h': [
-                    {foo: [1,2,3]},
-                    {foo: [4,5,6]},
-                    'blue',
-                    true
-                ]
-            }
-        };
-        t.deepEqual(transposer.eval(obj, '$.b.h[(@.length-2)]'), ['blue']);
-        t.deepEqual(transposer.eval(obj, '$.b.h[(@.length-1)]'), [true]);
-        t.deepEqual(transposer.eval(obj, '$.b.h[(@.length-3)]'), [{foo: [4,5,6]}]);
-        t.deepEqual(transposer.eval(obj, '$.b.h.length'), [4]);
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h[(@.length-2)]'));
         t.end();
     });
 
     t.test('should test conditionals using ? for existence', function(t) {
         var obj = {
-            'b': {
-                'h': [
-                    {foo: [1,2,3]},
-                    {foo: [4,5,6]},
-                    'blue',
-                    true
-                ]
-            }
-        };
-        t.deepEqual(transposer.eval(obj, '$.b.h[?(@.foo)]'), [[1,2,3],[4,5,6]]);
+                'b': {
+                    'h': [
+                        {foo: [1,2,3]},
+                        {foo: [4,5,6]},
+                        'blue',
+                        true
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h[?(@.foo)]');
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h[?(@.foo)]'));
         t.end();
     });
 
     t.test('should test conditionals using ? for comparison', function(t) {
-        var obj = {
-            'b': {
-                'h': [
-                    {foo: [1,2,3]},
-                    {foo: [4,5,6]},
-                    {foo: 12.9, name: 'a'},
-                    {foo: 13.5, name: 'b'},
-                    {foo: 11.8, name: 'c'},
-                    true,
-                    123,
-                    [3,4,5]
-                ]
-            }
-        };
-        //it breaks if i have the comparator value is 12.5 (brittle need to fix)
-        t.deepEqual(transposer.eval(obj, '$.b.h[?(@.foo>12)]'), [{foo:12.9, name: 'a'}, {foo:13.5, name: 'b'}]);
-        t.end();
-    });
-
-    t.test('transpose an object into another', function(t) {
         var obj = {
                 'b': {
                     'h': [
@@ -203,22 +219,17 @@ test('logger', function (t) {
                         {foo: 12.9, name: 'a'},
                         {foo: 13.5, name: 'b'},
                         {foo: 11.8, name: 'c'},
-                        {bar: 'tada'}
+                        true,
+                        123,
+                        [3,4,5]
                     ]
                 }
             },
-            spec = {
-                't_foo' : '$.b.h[?(@.foo>12)]',
-                't_h_length': '$.b.h.length',
-                't_h_lastEntry': '$.b.h[(@.length-1)]'
-            };
-        t.deepEqual(transposer.transpose(obj, spec), {
-            't_foo': [{foo:12.9, name: 'a'}, {foo:13.5, name: 'b'}],
-            't_h_length': [6],
-            't_h_lastEntry': [{bar: 'tada'}]
-        });
+            tr = new Transposer('$.b.h[?(@.foo>12)]');
+
+        //it breaks if i have the comparator value is 12.5 (brittle need to fix)
+        t.deepEqual(tr.eval(obj),  jsonPath.eval(obj, '$.b.h[?(@.foo>12)]'));
         t.end();
     });
-
-
 });
+
