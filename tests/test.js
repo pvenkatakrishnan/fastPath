04969ccbcf611d1c5a173f9e7f75a123a66ecf91
poornima <pvenkatakrishnan@paypal.com>
2014-08-19 18:26:46 -0700

    Adding more tests

--- tests/test.js
+++ tests/test.js
@@ -412,6 +412,65 @@ test('transposer', function (t) {
         t.end();
     });
 
+    t.test('should test .. operator with array length', function(t) {
+        var obj = {
+                'b': {
+                    'h': [
+                        {foo: [1,2,3]},
+                        {foo: [4,5,6]},
+                        {foo: 12.9, name: 'a'},
+                        {foo: 13.5, name: { 'h': 45}},
+                        {foo: 11.8, name: 'c'},
+                        true,
+                        123,
+                        [3,4,5]
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h[:2]');
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h[:2]'));
+        t.end();
+    });
+
+    t.test('should test .. operator with array length', function(t) {
+        var obj = {
+                'b': {
+                    'h': [
+                        {foo: [1,2,3]},
+                        {foo: [4,5,6]},
+                        {foo: 12.9, name: 'a'},
+                        {foo: 13.5, name: { 'h': 45}},
+                        {foo: 11.8, name: 'c'},
+                        true,
+                        123,
+                        [3,4,5]
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h[-2:]');
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h[-2:]'));
+        t.end();
+    });
+
+    t.test('should test .. operator with array length', function(t) {
+        var obj = {
+                'b': {
+                    'h': [
+                        {foo: [1,2,3]},
+                        {foo: [4,5,6]},
+                        {foo: 12.9, name: 'a'},
+                        {foo: 13.5, name: { 'h': 45}},
+                        {foo: 11.8, name: 'c'},
+                        true,
+                        123,
+                        [3,4,5]
+                    ]
+                }
+            },
+            tr = new Transposer('$.b.h[1:5:2]');
+        t.deepEqual(tr.eval(obj), jsonPath.eval(obj, '$.b.h[1:5:2]'));
+        t.end();
+    });
 
 });
 
