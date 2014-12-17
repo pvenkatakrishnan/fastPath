1d0b8a4d50e4459b7f35cbea0d68acacca9a874c
poornima <pvenkatakrishnan@paypal.com>
2014-12-16 21:22:56 -0800

    Fixing bug in named patterns

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var fastpath =  require('./lib/fastPath');
 
 module.exports = function(pattern) {
-    var res = {};
-
+    var input = {};
+    var output = {};
     if (typeof pattern === 'string') {
         //simple string patterns
         return fastpath(pattern);
@@ -12,15 +12,15 @@ module.exports = function(pattern) {
         //named patterns
         var keys = Object.keys(pattern);
         keys.forEach(function(key) {
-            res[key] = fastpath(pattern[key]);
+            input[key] = fastpath(pattern[key]);
         });
 
         return {
             evaluate: function(obj) {
                 keys.forEach(function(key) {
-                    res[key] = res[key].evaluate(obj);
+                    output[key] = input[key].evaluate(obj);
                 });
-                return res;
+                return output;
             }
         };
     }
